61670cbec05a51edf670205bec5c29294d0c060a
Weicheng <mingsnu@gmail.com>
2011-09-09 14:19:33 +0900

    sprintf instead of paste

--- economic1.R
+++ economic1.R
@@ -101,7 +101,7 @@ eco.xbar <- function(h=seq(0.1,1,by=.01),L=seq(2,4.5,by=.01),n=1:20,a1=1,a2=.1,W
   par(mar=c(7.1,4.1,2.1,2.1))
   contour(h,L,outer(h,L,FUN=f2,n=optimum[1]),xlab="h",ylab="L",...)
   points(optimum[3],optimum[2],pch=3)
-  mtext(paste("n=",optimum[1],", Opt L=", optimum[2], ", Opt h=",optimum[3], ", Cost=", round(optimum[4],digits=4)),side=1,line=4.5)
+  mtext(sprintf('n=%s   Opt L=%s   Opt h=%s   Cost=%s',optimum[1], optimum[2], optimum[3],round(optimum[4],digits=4)),side=1,line=4.5)
   return(list(optimum,cost.frame))
 }
 
